25c96dbbe24ee2c4d057c1619d40c05ff204b742
andywang505 <andywang890505@gmail.com>
2024-08-29 20:40:06 +0800

    feat: update meta info

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'AndyWang′s Blog';
-export const SITE_DESCRIPTION = 'Welcome to my Blog!';
+export const SITE_DESCRIPTION = '這裡是 AndyWang 的部落格，或者也可以叫我 weiwei，這裡記錄著我的開發經驗以及技術筆記相關文章。';
