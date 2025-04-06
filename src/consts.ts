7c8af3e1c935873a9eef3f98bc22482dc1d05061
andywang505 <andywang890505@gmail.com>
2025-04-06 23:53:43 +0800

    feat: update site metadata and default images

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,12 @@
 
 export const SITE_TITLE = 'AndyWang′s Blog';
 export const SITE_DESCRIPTION =
-  '這裡是 AndyWang 的部落格，或者也可以叫我 weiwei，這裡記錄著我的開發經驗以及技術筆記相關文章。';
+  '這裡是 AndyWang 的部落格，主要記錄著我的開發經驗以及技術筆記相關文章，同時也會分享或探討一些技術議題。';
+
+// Default images
+export const DEFAULT_BLOG_IMAGE = '/SITCON2025.png';
+export const DEFAULT_AVATAR = 'https://avatars.githubusercontent.com/u/71600455?v=4';
+
+// Footer background images
+export const FOOTER_BG_LIGHT = '/public/back-footer/cave.foothold1.1.0.png';
+export const FOOTER_BG_DARK = '/public/back-footer/enH1.1.png';
