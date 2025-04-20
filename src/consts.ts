88d4cc7beda49434bd4a38c59fd9293afe112fa0
andywang505 <andywang890505@gmail.com>
2025-04-20 19:07:12 +0800

    chore: update blog constants and image sources for consistency

--- src/consts.ts
+++ src/consts.ts
@@ -3,10 +3,10 @@
 
 export const SITE_TITLE = 'AndyWang′s Blog';
 export const SITE_DESCRIPTION =
-  '這裡是 AndyWang 的部落格，主要記錄著我的開發經驗以及技術筆記相關文章，同時也會分享或探討一些技術議題。';
+  '這裡是 AndyWang 的部落格，主要記錄著我的開發經驗以及技術筆記相關文章，偶爾也會分享或討論一些技術議題。';
 
 // Default images
-export const DEFAULT_BLOG_IMAGE = '/SITCON2025.png';
+export const DEFAULT_BLOG_IMAGE = 'https://avatars.githubusercontent.com/u/71600455?v=4';
 export const DEFAULT_AVATAR = 'https://avatars.githubusercontent.com/u/71600455?v=4';
 
 // Footer background images
